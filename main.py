f46325689fa6dd045168e3b008d7221bee9b6a7f
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-01-27 23:44:44 +0500

    bye

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def run():
                         print(publish['description'])
                     else:
                         print("Successfully downloaded :)")
-
+print("bye")
 
 if __name__ == '__main__':
     run()
